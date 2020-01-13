4de2a4a5c6b04474d4affb3d0d6206924d9a781c
Vincent Post <v.post@condifood.com>
2020-01-13 20:58:55 +0100

    ADD: set and return process pid

--- include/process.h
+++ include/process.h
@@ -8,7 +8,8 @@ It contains relevant attributes as shown below
 */
 class Process {
 public:
-  int Pid();                               // TODO: See src/process.cpp
+  int Pid();                               // DONE: See src/process.cpp
+  void Pid(int pid);
   std::string User();                      // TODO: See src/process.cpp
   std::string Command();                   // TODO: See src/process.cpp
   float CpuUtilization();                  // TODO: See src/process.cpp
@@ -18,6 +19,8 @@ public:
 
   // TODO: Declare any necessary private members
 private:
+  int pid_;
+  std::string user_;
 };
 
 #endif
--- src/process.cpp
+++ src/process.cpp
@@ -11,8 +11,22 @@ using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+// DONE: Return this process's ID
+int Process::Pid()
+{
+  return pid_;
+}
+
+void Process::Pid(int pid)
+{
+  pid_ = pid;
+}
+
+// TODO: Return the user (name) that generated this process
+string Process::User()
+{
+  return string();
+}
 
 // TODO: Return this process's CPU utilization
 float Process::CpuUtilization() { return 0; }
@@ -23,8 +37,6 @@ string Process::Command() { return string(); }
 // TODO: Return this process's memory utilization
 string Process::Ram() { return string(); }
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
 
 // TODO: Return the age of this process (in seconds)
 long int Process::UpTime() { return 0; }
