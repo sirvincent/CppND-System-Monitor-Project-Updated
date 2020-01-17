3a9534634bee01bca5a32a6387a055459f9b44f6
Vincent Post <v.post@condifood.com>
2020-01-17 08:27:58 +0100

    DEL: comments

--- src/processor.cpp
+++ src/processor.cpp
@@ -13,23 +13,3 @@ float Processor::Utilization()
 
   return cpuUsage;
 }
-
-/*
-Utilization calculation:
-https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
-
-PrevIdle = previdle + previowait
-Idle = idle + iowait
-
-PrevNonIdle = prevuser + prevnice + prevsystem + previrq + prevsoftirq + prevsteal
-NonIdle = user + nice + system + irq + softirq + steal
-
-PrevTotal = PrevIdle + PrevNonIdle
-Total = Idle + NonIdle
-
-# differentiate: actual value minus the previous one
-totald = Total - PrevTotal
-idled = Idle - PrevIdle
-
-CPU_Percentage = (totald - idled)/totald
-*/
