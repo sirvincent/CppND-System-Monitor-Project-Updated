2aadd89cc9c7a34f1f972bc63d7da9b30ba71812
Vincent Post <v.post@condifood.com>
2020-01-21 20:49:09 +0100

    RFT: reviewers much simpler ElapsedTime using iomanip

--- src/format.cpp
+++ src/format.cpp
@@ -3,46 +3,29 @@
 #include <algorithm>
 #include <cassert>
 #include <string>
+#include <iomanip>
 
-using std::string;
-
-void Format::formatNumber(string &number) {
-  if (number.size() == 1) {
-    number = string("0") + number;
-  }
-}
 
 // DONE: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-string Format::ElapsedTime(long seconds) {
+std::string Format::ElapsedTime(long secondsTotal) {
+
   // display can only show up to 99 hours
-  if (seconds > 360000) {
-    seconds = seconds % 360000;
+  if (secondsTotal > 360000) {
+    secondsTotal = secondsTotal % 360000;
   }
 
-  long hours = seconds / 3600;
-  if (hours > 0) {
-    seconds = seconds % 3600;
-  }
-  long minutes = seconds / 60;
-  if (minutes > 0) {
-    seconds = seconds % 60;
-  }
+  long hours = secondsTotal / 3600;
+  long minutes = (secondsTotal % 3600) / 60;
+  long seconds = (secondsTotal % 3600 % 60);
 
-  // TODO: this is messy how to make this nicer? std::format (C++20), sprintf?
-  string hourString = std::to_string(hours);
-  formatNumber(hourString);
-  string minutesString = std::to_string(minutes);
-  formatNumber(minutesString);
-  string secondsString = std::to_string(seconds);
-  formatNumber(secondsString);
-  // similar things should be in tests
-  assert(hourString.size() == 2);
-  assert(minutesString.size() == 2);
-  assert(secondsString.size() == 2);
+  //setw and setfill for adding leading zeros
+  std::ostringstream hourStream, minuteStream, secondStream;
+  hourStream << std::setw(2) << std::setfill('0') << hours;
+  minuteStream << std::setw(2) << std::setfill('0') << minutes;
+  secondStream << std::setw(2) << std::setfill('0') << seconds;
 
-  string elapsedTime = hourString + ":" + minutesString + ":" + secondsString;
-
+  std::string elapsedTime = hourStream.str() + ":" + minuteStream.str() + ":" + secondStream.str();
   return elapsedTime;
 }
